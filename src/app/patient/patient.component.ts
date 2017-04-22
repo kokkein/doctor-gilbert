a91a36978ba73333f5d87a6fbb496be5eee0a478
kokkein <kokkein@hotmail.com>
2017-04-23 00:41:53 +0800

    23 apr

--- src/app/patient/patient.component.ts
+++ src/app/patient/patient.component.ts
@@ -16,8 +16,32 @@ export class PatientComponent implements OnInit {
   titles;
   IDtypes;
   relationship;
+  payors;
   states;
+  
   selectedCountry= 190;
+  reactiveStates: any;
+
+  tdDisabled = false;
+
+
+//payors =
+//[
+//  {payorID:2,payorName:'Great Eastern Life Assurance (Malaysia) Berhad',phone:'03-4259 8888',fax:'03-4259 8000',email:'wecare-my@greateasternlife.com',address:'Menara Great Eastern, 303, Jalan Ampang',city:'',state:'Kuala Lumpur',zipcode:50450,countryID:381},
+//  {payorID:3,payorName:'Allianz Life Insurance Malaysia Berhad',phone:'03-2264 1188',fax:'03-2264 1199',email:'Customer.service@allianz.com.my',address:'Level 29, Menara Allianz Sentral, 203, Jalan Tun Sambanthan, Kuala Lumpur Sentral',city:'',state:'Kuala Lumpur',zipcode:50470,countryID:381},
+//  {payorID:4,payorName:'AmMetLife Insurance Berhad',phone:'03-2271 8000',fax:'03-2272 3230',email:'customercare@ammetlife.com',address:'Level 24, Menara 1 Sentrum, No.201 , Jalan Tun Sambanthan',city:'',state:'Kuala Lumpur',zipcode:50470,countryID:381},
+//  {payorID:5,payorName:'AIA Bhd.',phone:'03-2056 1111',fax:'03-2056 2992',email:'corporate-ops.my@aia.com',address:'Menara AIA, 99 Jalan Ampang',city:'',state:'Kuala Lumpur',zipcode:50450,countryID:381},
+//  {payorID:6,payorName:'AXA AFFIN Life Insurance Berhad',phone:'03-2117 6688',fax:'03-2117 6768',email:'customer.care@axa-life.com.my',address:'8th Floor Chulan Tower, 3 Jalan Conlay',city:'',state:'Kuala Lumpur',zipcode:50450,countryID:381},
+//  {payorID:7,payorName:'Etiqa Insurance Berhad',phone:'03-2297 3888',fax:'03-2297 3800',email:'info@etiqa.com.my',address:'Level 19, Tower C, Dataran Maybank, No. 1, Jalan Maarof',city:'',state:'Kuala Lumpur',zipcode:59000,countryID:381},
+//  {payorID:8,payorName:'Etiqa Insurance Berhad',phone:'03-2297 3888',fax:'03-2297 3800',email:'info@etiqa.com.my',address:'Level 19, Tower C, Dataran Maybank, No. 1, Jalan Maarof',city:'',state:'Kuala Lumpur',zipcode:59000,countryID:381},
+//  {payorID:9,payorName:'Manulife Insurance Berhad',phone:'03-2297 3888',fax:'03-2297 3800',email:'info@etiqa.com.my',address:'Level 19, Tower C, Dataran Maybank, No. 1, Jalan Maarof',city:'',state:'Kuala Lumpur',zipcode:59000,countryID:381},
+//  {payorID:10,payorName:'MCIS Insurance Berhad',phone:'03-2297 3888',fax:'03-2297 3800',email:'info@etiqa.com.my',address:'Level 19, Tower C, Dataran Maybank, No. 1, Jalan Maarof',city:'',state:'Kuala Lumpur',zipcode:59000,countryID:381},
+//  {payorID:11,payorName:'Prudential Assurance Malaysia Berhad',phone:'03-2297 3888',fax:'03-2297 3800',email:'info@etiqa.com.my',address:'Level 19, Tower C, Dataran Maybank, No. 1, Jalan Maarof',city:'',state:'Kuala Lumpur',zipcode:59000,countryID:381},
+//  {payorID:12,payorName:'Sun Life Malaysia Assurance Berhad',phone:'03-2297 3888',fax:'03-2297 3800',email:'info@etiqa.com.my',address:'Level 19, Tower C, Dataran Maybank, No. 1, Jalan Maarof',city:'',state:'Kuala Lumpur',zipcode:59000,countryID:381},
+//  {payorID:13,payorName:'Tokio Marine Life Insurance Malaysia Bhd.',phone:'03-2297 3888',fax:'03-2297 3800',email:'info@etiqa.com.my',address:'Level 19, Tower C, Dataran Maybank, No. 1, Jalan Maarof',city:'',state:'Kuala Lumpur',zipcode:59000,countryID:381},
+//  {payorID:14,payorName:'Gibraltar BSN Life Berhad',phone:'03-2297 3800',fax:'03-2297 3800',email:'info@etiqa.com.my',address:'Level 19, Tower C, Dataran Maybank, No. 1, Jalan Maarof',city:'',state:'Kuala Lumpur',zipcode:59000,countryID:381},
+//  {payorID:15,payorName:'Zurich Insurance Malaysia Berhad',phone:'03-2297 3888',fax:'03-2297 3800',email:'info@etiqa.com.my',address:'Level 19, Tower C, Dataran Maybank, No. 1, Jalan Maarof',city:'',state:'Kuala Lumpur',zipcode:59000,countryID:381}
+//];
 
   insuranceCtrl: FormControl;
   filteredInsurances: any;
@@ -40,16 +64,6 @@ insurances = [
     'Discovery Comprehensive',
   ];
 
-payors = [
-    'AIA Sdn Bhd',
-    'Great Estern Sdn Bhd',
-    'Maybank Takaful Sdn Bhd',
-    'Maybank Berhad',
-    'City Bank',
-    'Compu Group Medical Sdn Bhd',
-    'HSBC Berhad',
-    'Petronas Berhad',
-  ];
 
 
   constructor(private MDS: MasterDataService) {
@@ -58,10 +72,13 @@ payors = [
         .startWith(null)
         .map(name => this.filterInsurances(name));
 
-    this.payorCtrl = new FormControl();
-    this.filteredPayors = this.payorCtrl.valueChanges
-        .startWith(null)
-        .map(name => this.filterPayors(name));
+    //assign value for edit mode
+    this.payorCtrl = new FormControl({payorID: 13, payorName: 'Tokio Marine Life Insurance Malaysia Bhd.'});
+
+  }
+
+  displayFn(value: any): string {
+    return value && typeof value === 'object' ? value.payorName : value;
   }
 
   filterInsurances(val: string) {
@@ -69,7 +86,8 @@ payors = [
   }
 
   filterPayors(val: string) {
-    return val ? this.payors.filter((s) => new RegExp(val, 'gi').test(s)) : this.payors;
+    return val ? this.payors.filter((s) => new RegExp(`^${val}`, 'gi').test(s.payorName))
+               : this.payors;
   }
 
   ngOnInit() {
@@ -82,8 +100,16 @@ payors = [
     //this.MDS.GetInsurance().subscribe(insurance => {
     //this.insurances = insurance;});
 
-    //this.MDS.GetPayor().subscribe(payor => {
-    //this.payors = payor;});
+    this.MDS.GetPayor().subscribe(payor => {
+    this.payors = payor;
+
+    //here only start filter
+    this.filteredPayors = this.payorCtrl.valueChanges
+        .startWith(this.payorCtrl.value)
+        .map(val => this.displayFn(val))
+        .map(name => this.filterPayors(name));
+
+    });
 
     this.MDS.GetRelationship().subscribe(relationship => {
     this.relationship = relationship;});
